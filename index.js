48584be074a0be4bd2da3067c8c0870d63f8f39c
Ishita Jain <ishitajain385@gmail.com>
2025-03-22 18:03:25 +0530

    added a few css changes and dynamic images coming via random method

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
  function filterImages(filter){
     console.log(filter);
  }
+
+let NatureArray=["assets/nature/natureimg1.jpg","assets/nature/natureimg2.jpg", "assets/nature/natureimg3.jpg","assets/nature/natureimg4.jpg" , "assets/nature/natureimg5.jpg"]
+
+let card2= document.querySelector('.card-2')
+for(let i=0;i<=3;i++){
+   let img= document.createElement('img')
+   img.className= 'card-2-'+i
+   card2.append(img)
+   console.log(img);
+}
+console.log(card2.childNodes);
+
+function natureImages(){
+   for(let i=0; i<NatureArray.length;i++){
+    let img= document.querySelector(`.card-2-${i}`)
+
+    img.src= NatureArray[Math.floor(Math.random()*NatureArray.length)]
+    console.log(img);
+   }
+}
+
+natureImages()
